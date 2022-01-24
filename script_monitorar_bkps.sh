61493478289e787e05e9ac3e50a7f497dcef78f0
Saulo Costa <saulotarsobc@gmail.com>
2022-01-24 11:26:45 -0300

    Update script_monitorar_bkps.sh

--- script_monitorar_bkps.sh
+++ script_monitorar_bkps.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-names=($(ls /var/www/bkp -ltr --time-style='+%s' | grep "\.ixc" |sed -r -e 's/.*(BKP.*)/\1/'));
-sizes=($(ls /var/www/bkp -ltr --time-style='+%s' | grep "\.ixc" |sed -r -e 's/.*data\s([0-9]+)\s.*/\1/'));
-dates=($(ls /var/www/bkp -ltr --time-style='+%s' | grep "\.ixc" |sed -r -e 's/.*\s([0-9]{10})\sBKP.*/\1/'));
+extension=".ixc";
+names=($(ls /var/www/bkp -ltr --time-style='+%s' | grep "$extension" |sed -r -e 's/.*(BKP.*)/\1/'));
+sizes=($(ls /var/www/bkp -ltr --time-style='+%s' | grep "$extension" |sed -r -e 's/.*data\s([0-9]+)\s.*/\1/'));
+dates=($(ls /var/www/bkp -ltr --time-style='+%s' | grep "$extension" |sed -r -e 's/.*\s([0-9]{10})\sBKP.*/\1/'));
 
 ID=0;
 timestamp=$(date '+%s');
