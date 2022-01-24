1bcba2cd0716a073f14f735240504f19f37282f0
Saulo Costa <saulotarsobc@gmail.com>
2022-01-24 11:52:45 -0300

    :fire:

--- script_monitorar_bkps.sh
+++ script_monitorar_bkps.sh
@@ -1,15 +1,3 @@
 #! /bin/bash
-extension=".ixc";
-names=($(ls /var/www/bkp -ltr --time-style='+%s' | grep "$extension" |sed -r -e 's/.*(BKP.*)/\1/'));
-sizes=($(ls /var/www/bkp -ltr --time-style='+%s' | grep "$extension" |sed -r -e 's/.*data\s([0-9]+)\s.*/\1/'));
-dates=($(ls /var/www/bkp -ltr --time-style='+%s' | grep "$extension" |sed -r -e 's/.*\s([0-9]{10})\sBKP.*/\1/'));
-
-ID=0;
-timestamp=$(date '+%s');
-
-echo "id|nome|size|uptime";
-for i in "${names[@]}"; do
-    uptime=$(( timestamp - ${dates[$ID]} ));
-    echo "$ID|${names[$ID]}|${sizes[$ID]}|$uptime";
-    let ID=ID+1;
-done;
+ex=".ixc"; names=($(ls /var/www/bkp -ltr --time-style='+%s' |grep "$ex" |sed -r -e 's/.*\s[0-9]+\s[0-9]+\s(.*)/\1/')); sizes=($(ls /var/www/bkp -ltr --time-style='+%s' |grep "$ex" |sed -r -e 's/.*\s([0-9]+)\s[0-9]+\s.*/\1/')); dates=($(ls /var/www/bkp -ltr --time-style='+%s' |grep "$ex" |sed -r -e 's/.*\s[0-9]+\s([0-9]+)\s.*/\1/')); ID=0; timestamp=$(date '+%s'); echo "id|nome|size|uptime"; for i in "${names[@]}"; do
+    uptime=$(( timestamp - ${dates[$ID]} ));     echo "$ID|${names[$ID]}|${sizes[$ID]}|$uptime";     let ID=ID+1; done; 
